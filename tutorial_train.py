e83289fe5464aa2a02f563c9c3b64c748828f6f4
ydniuyongjie <niuyongjie@163.com>
2025-08-01 10:54:44 +0000

    ff

--- tutorial_train.py
+++ tutorial_train.py
@@ -9,7 +9,7 @@ from cldm.model import create_model, load_state_dict
 
 # Configs
 resume_path = './models/control_sd15_ini.ckpt'
-batch_size = 4
+batch_size = 1
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
