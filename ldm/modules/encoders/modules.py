8ddd70bbcdc812e56a7c7993116fbc55e1fb7f65
ydniuyongjie <niuyongjie@163.com>
2025-08-01 08:51:29 +0000

    clip-vit-large-patch14 is local path

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -92,7 +92,7 @@ class FrozenCLIPEmbedder(AbstractEncoder):
         "pooled",
         "hidden"
     ]
-    def __init__(self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77,
+    def __init__(self, version="/home/tniuyj/code/ControlNet/ldm/modules/encoders/clip-vit-large-patch14", device="cuda", max_length=77,
                  freeze=True, layer="last", layer_idx=None):  # clip-vit-base-patch32
         super().__init__()
         assert layer in self.LAYERS
