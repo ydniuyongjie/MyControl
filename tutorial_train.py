bdef86edea6777fa4b63e6bc9c8808d1ef6de21e
ydniuyongjie <niuyongjie@163.com>
2025-08-15 16:25:28 +0000

    modify code

--- tutorial_train.py
+++ tutorial_train.py
@@ -3,7 +3,7 @@ from share import *
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from tutorial_dataset import MyDataset
-from cldm.logger import ImageLogger
+from clam.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
 
 
